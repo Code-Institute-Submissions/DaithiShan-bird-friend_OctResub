c34d54477c8815a557daec3d15adbc4f972f39bf
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-04 16:31:40 +0000

    Navbar added, bug identified therein

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+
+    colors: {
+      gorm: {
+        '100': '#1A535C'
+      },
+      glas: {
+        '100': '#4ECDC4'
+      },
+      ban: {
+        '100': '#F7FFF7'
+      },
+      rua: {
+        '100': '#FF6B6B'
+      },
+      bui: {
+        '100': '#FFE66D'
+      },
+      liath: {
+        '100': '#504A5C'
+      },
+    },
+    fontFamily: {
+      welcome: ['Jaldi'],
+      title: ['Roboto Slab'],
+      body: ['Lato']
+    }
   },
   variants: {
     extend: {},
