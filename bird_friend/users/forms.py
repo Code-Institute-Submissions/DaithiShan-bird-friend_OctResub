c969a8fa37ae4048bec1e23f72cdd59a5a1d5ef4
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-07 19:52:07 +0000

    Registration form added using User model

--- bird_friend/users/forms.py
+++ bird_friend/users/forms.py
@@ -2,8 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, \
     Length
+from bird_friend.models import User
+from flask_login import current_user
 
-#Customised registration form class from student project - https://github.com/cjcon90/hot-dogz
+#Registration form class from student project - https://github.com/cjcon90/hot-dogz/blob/main/hot_dogz/users/forms.py
 
 class RegisterForm(FlaskForm):
     username = StringField('Username',
@@ -31,3 +33,15 @@ class RegisterForm(FlaskForm):
             EqualTo('password', message="Passwords don't match! "
                                         "Please try again")])
     submit = SubmitField('Register')
+
+    # Function to check that username isn't already registered on site
+    def validate_username(self, username):
+        existing = User.objects(username=username.data).first()
+        if existing is not None:
+            raise ValidationError('Please use a different username.')
+
+    # Function to check that email isn't already registered on site
+    def validate_email(self, email):
+        existing = User.objects(email=email.data).first()
+        if existing is not None:
+            raise ValidationError('Please use a different email.')
