5b9aa0d7fc6a1efec83e030a6a2eb9d651d26f47
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-07 19:08:09 +0000

    Config file added

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+import cloudinary
+if os.path.exists("env.py"):
+    import env
+
+class Config(object):
+    # Flask Secret Key
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # Mongo DB Settings
+    MONGODB_SETTINGS = {
+        'db': os.environ.get('MONGO_DBNAME'),
+        'host': os.environ.get('MONGO_URI')
+    }
+    # Cloudinary API Settings
+    cloudinary.config(
+        cloud_name=os.environ.get("CLOUD_NAME"),
+        api_key=os.environ.get("CLOUD_API_KEY"),
+        api_secret=os.environ.get("CLOUD_API_SECRET")
+    )
+    # Email configuration
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMIN = ['david.shanahan.burns@gmail.com']
