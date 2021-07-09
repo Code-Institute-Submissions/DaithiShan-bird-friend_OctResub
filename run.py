90cd944594a0b9729e1ad402c955c55afc484647
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-09 21:40:02 +0000

    Test landing page Blueprint added to run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import os
+import env
 from bird_friend import create_app, db
 from bird_friend import users
 
 app = create_app()
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
