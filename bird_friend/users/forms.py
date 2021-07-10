a131eb664de5f189c58816ab6dbe760b28ca60e5
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-10 15:31:31 +0000

    Added error and flash messages, squashed bug in registration template for loop

--- bird_friend/users/forms.py
+++ bird_friend/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, \
-    Length
+from wtforms.validators import (
+    DataRequired, Email, EqualTo, ValidationError, Length)
 from bird_friend.models import User
 from flask_login import current_user
 
