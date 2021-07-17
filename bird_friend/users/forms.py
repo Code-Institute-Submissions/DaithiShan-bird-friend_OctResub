7bde0db5d64ad507dc6691e14a670b90537d005f
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-17 12:03:15 +0000

    Edit Profile Form and Delete Account form added to app

--- bird_friend/users/forms.py
+++ bird_friend/users/forms.py
@@ -56,3 +56,40 @@ class RegisterForm(FlaskForm):
         existing = User.objects(email=email.data).first()
         if existing is not None:
             raise ValidationError('Please use a different email.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[
+                               DataRequired(message="Please enter a username"),
+                               Length(min=2, max=12,
+                                      message="Please enter a username "
+                                              "between "
+                                              "2 and 12 characters long")])
+    email = StringField(
+        'Email', validators=[
+            DataRequired(message="Please enter an email"),
+            Email(message="Please enter a valid email address"),
+            Length(max=100, message="Email is too long "
+                                    "(100 character max)")])
+    submit = SubmitField('Submit')
+
+    # Function to check that username (if new) isn't already registered on site
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            existing = User.objects(username=username.data).first()
+            if existing is not None:
+                raise ValidationError('Please use a different username.')
+
+    # Function to check that email (if new) isn't already registered on site
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            existing = User.objects(email=email.data).first()
+            if existing is not None:
+                raise ValidationError('Please use a different email.')
+
+
+class DeleteAccountForm(FlaskForm):
+    password = PasswordField('Confirm Password', validators=[
+        DataRequired(message="Please enter a password")])
+    submit = SubmitField('Delete Account')
