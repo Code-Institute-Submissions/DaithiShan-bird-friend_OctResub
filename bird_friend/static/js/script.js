2c629e31773269c62f008678b302d4dce445c9ad
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-21 19:51:36 +0000

    Added template FadeIn function for gallery animate

--- bird_friend/static/js/script.js
+++ bird_friend/static/js/script.js
@@ -5,7 +5,15 @@ if(galleryContainer) {
   // if true, perform opacity animation on each child element
   let birdCards = galleryContainer.children;
   
-  for (let i = 0; i < dogCards.length; i++){
+  for (let i = 0; i < birdCards.length; i++){
     fadeIn(i, birdCards[i]);
   }
+}
+
+// Function for adding a delay within a loop
+// Souce: https://www.geeksforgeeks.org/how-to-add-a-delay-in-a-javascript-loop/
+function fadeIn(idx, e){
+  setTimeout(function() {
+    e.classList.remove('opacity-0');
+  }, 500 + (250 * idx));
 }
