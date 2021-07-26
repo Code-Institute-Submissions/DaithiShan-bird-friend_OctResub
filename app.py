77cff2954b544eb1c0d5dc1f9ee2292f7ac10aa9
DaithiShan <david.shanahan.burns@gmail.com>
2021-07-26 17:39:59 +0000

    Run.py renamed to app.py to fix Heroku deployment bug

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import env
+from bird_friend import create_app, db
+from bird_friend.models import User, Birdtype
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User,
+            'Bird': Bird, 'Birdtype': Birdtype}
