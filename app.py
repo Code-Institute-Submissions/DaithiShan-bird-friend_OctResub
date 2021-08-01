6f9869ab32e3ca1554876d9f05349102b0e028ed
DaithiShan <david.shanahan.burns@gmail.com>
2021-08-01 07:45:24 +0000

    Updating python files to ensure PEP8 compliance

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
         port=int(os.environ.get("PORT")),
         debug=True)
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User,
