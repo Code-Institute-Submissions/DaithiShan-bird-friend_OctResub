2f20510ee514a029f06a12774f71a148e75762f0
DaithiShan <david.shanahan.burns@gmail.com>
2021-08-01 07:46:58 +0000

    Updating python files to ensure PEP8 compliance 3/3

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
 
 
 @app.shell_context_processor
